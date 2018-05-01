af4147b6c21f6d88e17f5263c49f1c35a5c45759
tamirkash <tamirkash@gmail.com>
2018-05-01 20:23:49 +0300

    remove unnecessary tab readiness status check

--- background.js
+++ background.js
@@ -1,14 +1,7 @@
-var previousURL = "";
 var rxLookfor = /(?:linkedin\.com\/jobs\/search\/)/;
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if(changeInfo.status === 'complete' && rxLookfor.test(previousURL)){
-    	previousURL = "";
-    	chrome.tabs.sendMessage(tabId, 'url-update');
-    } else if (rxLookfor.test(changeInfo.url)) {
-        previousURL = changeInfo.url;
+    if (rxLookfor.test(changeInfo.url)) {
+        chrome.tabs.sendMessage(tabId, 'url-update');
     }
 });
-// chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-//     chrome.tabs.executeScript(null,{file:"contents.js"});
-// });
